83804bf32a9e11810d16c3923cbee2cd589e0e67
ronaldtrujillo2108 <cocollante2108q@gmail.com>
2025-07-05 14:47:39 -0400

    hola

--- Modulo 2/Calculadora/script.js
+++ Modulo 2/Calculadora/script.js
@@ -1,28 +1,23 @@
 const othersOp = document.getElementById("othersOp");
 const numbers = document.getElementById("numbers");
 
-
 const OTHERS_OP = [
- "X","Calc", "","", "Pi","x","ab",
- "√", "x²", "^", "log", "In", 
+  "X", "Calc", "", "", "Pi", "x³", "ab", 
+  "√", "x²", "^", "log", "In", 
   "(-)", ".,,,", "hyp", "sin", "cos", 
   "tan", "Rc1", "Eng", "(",
   ")", ",", "m+"
 ]
-
 const NUMBERS = [
   "7", "8", "9", "DEL", "AC", 
   "4", "5", "6", "*", "/", 
   "1", "2", "3", "+", "-", 
   "0", ".", "EXP", "Ans",
-  "="
-]
+  "="]
 
 for (let index = 0; index < OTHERS_OP.length; index++) {
-    const button = document.createElement("button")
+    const button = document.createElement("button");
     button.innerText = OTHERS_OP[index];
-    othersOp.appendChild[button]
+    othersOp.appendChild[button];
     
-
-
 }
