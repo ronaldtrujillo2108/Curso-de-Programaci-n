0ac48ebc7a2661278772f915bf2010c09c22ed60
ronaldtrujillo2108 <cocollante2108@gmail.com>
2025-07-08 16:56:05 -0400

    hola

---
+++ Modulo 3/Ejercicio de PhP/Ejercicios C++/Practica.cpp
@@ -0,0 +1,20 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() {
+    std::cout <<"¡Hola mundo" << "Try programiz.pro";
+
+int numero = 8;   
+float numero_2 = 1.75;
+char character = 'A';
+bool Esverdadero = true;
+    
+    std::cout << "numero: " << numero << std::endl;
+    std::cout << "Float: " << numero_2 << std::endl;
+    std::cout << "Char: " << character << std::endl;
+    std::cout << "bool: " << Esverdadero << std::endl;
+    
+    return 0;
+}
+
+
